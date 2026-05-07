037dfc31faca309eab38b3080e5fb842a8957681
Valentina <vmunozvaleria@icloud.com>
2026-05-07 04:01:11 +0000

    update: vars and modules

--- EA2/ACT2.6/vpc_module/main.tf
+++ EA2/ACT2.6/vpc_module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "mi_vpc" {
   cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
   tags = {
     Name = var.vpc_name
   }
